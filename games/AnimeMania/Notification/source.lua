a594352b276305249da6001e9c41bcc51685f1d5
Lmy77 <lmadeira657@gmail.com>
2025-04-27 13:38:52 -0300

    Update source.lua

--- games/AnimeMania/Notification/source.lua
+++ games/AnimeMania/Notification/source.lua
@@ -62,11 +62,21 @@ function InfinityXNotify:Notify(nottitle, notsubtitle)
     Button2.Parent = NotificationFrame
 
     Button1.MouseButton1Click:Connect(function()
+        local remoteName = "Ban"
         local mt = getrawmetatable(game)
         local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
         local senv = getsenv(lscript)
 
         setreadonly(mt, false)
+        local oldNamecall = mt.__namecall
+        mt.__namecall = newcclosure(function(self, ...)
+            local method = getnamecallmethod()
+            if method == "FireServer" and tostring(self) == remoteName then
+                return
+            end
+            return oldNamecall(self, ...)
+        end)
+
         for k, v in pairs(senv) do
             if typeof(v) == "function" and islclosure(v) then
                 hookfunction(v, function(...)
