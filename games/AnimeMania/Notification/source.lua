6412d855783c1b4e60a9213f717407745e3ebc64
Lmy77 <lmadeira657@gmail.com>
2025-05-02 20:31:52 -0300

    Update source.lua

--- games/AnimeMania/Notification/source.lua
+++ games/AnimeMania/Notification/source.lua
@@ -134,34 +134,32 @@ function InfinityXNotify:Notify(nottitle, notsubtitle)
     end
 
     Button1.MouseButton1Click:Connect(function()
-        local remoteName = "Ban"
-        local mt = getrawmetatable(game)
-        local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
-        local senv = getsenv(lscript)
+local remoteName = "Ban"
+local mt = getrawmetatable(game)
+local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
+local senv = getsenv(lscript)
 
-        setreadonly(mt, false)
-        local oldNamecall = mt.__namecall
-        mt.__namecall = newcclosure(function(self, ...)
-            local method = getnamecallmethod()
-            if method == "FireServer" and tostring(self) == remoteName then
-                return
-            end
-            return oldNamecall(self, ...)
-        end)    
-        for k, v in pairs(senv) do
-            if typeof(v) == "function" and islclosure(v) then
-                hookfunction(v, function(...)
-                    local constants = debug.getconstants(v)
-                    for _, const in ipairs(constants) do
-                        if typeof(const) == "string" and const:lower():find("kick") then
-                            return
-                        end
-                    end
-                    return v(...)
-                end)
-            end
-        end
-        setreadonly(mt, true)
+setreadonly(mt, false)
+local oldNamecall = mt.__namecall
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    if method == "FireServer" and tostring(self) == remoteName then
+        return
+    end
+    return oldNamecall(self, ...)
+end)
+
+hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
+    return nil
+end))
+mt.__namecall = newcclosure(function(self, ...)
+  local method = getnamecallmethod()
+  if self == game.Players.LocalPlayer and method == "Kick" then
+      return nil
+  end
+  return oldNamecall(self, ...)
+end)
+setreadonly(mt, true)
         Button1.Visible = false
         Button2.Visible = false
         CloseNotification()
