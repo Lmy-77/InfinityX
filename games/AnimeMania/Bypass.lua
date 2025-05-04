9b0104d913fa332fed50e2c04d0a11cf832739c3
Lmy77 <lmadeira657@gmail.com>
2025-05-04 17:05:57 -0300

    Update Bypass.lua

--- games/AnimeMania/Bypass.lua
+++ games/AnimeMania/Bypass.lua
@@ -1,31 +1,28 @@
-if hookmetamethod and hookfunction and getsenv and newcclosure and getnamecallmethod and getrawmetatable then
-	local remoteName = "Ban"
-	local mt = getrawmetatable(game)
-	local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
-	local senv = getsenv(lscript)
+        if hookmetamethod and hookfunction and getsenv and newcclosure and getnamecallmethod and getrawmetatable then
+            local remoteName = "Ban"
+            local mt = getrawmetatable(game)
+            local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
+            local senv = getsenv(lscript)
 
-	setreadonly(mt, false)
-	local oldNamecall = mt.__namecall
-	mt.__namecall = newcclosure(function(self, ...)
-	    local method = getnamecallmethod()
-	    if method == "FireServer" and tostring(self) == remoteName then
-	        return
-	    end
-	    return oldNamecall(self, ...)
-	end)
+            setreadonly(mt, false)
+            local oldNamecall = mt.__namecall
+            mt.__namecall = newcclosure(function(self, ...)
+                local method = getnamecallmethod()
+                if method == "FireServer" and tostring(self) == remoteName then
+                    return
+                end
+                return oldNamecall(self, ...)
+            end)
 
-	hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
-	    return nil
-	end))
-	mt.__namecall = newcclosure(function(self, ...)
-	  local method = getnamecallmethod()
-	  if self == game.Players.LocalPlayer and method == "Kick" then
-	      return nil
-	  end
-	  return oldNamecall(self, ...)
-	end)
-	setreadonly(mt, true)
-else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/AnimeMania/Notification/return.lua", true))()
-	return
-end
+            hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
+                return nil
+            end))
+            mt.__namecall = newcclosure(function(self, ...)
+              local method = getnamecallmethod()
+              if self == game.Players.LocalPlayer and method == "Kick" then
+                  return nil
+              end
+              return oldNamecall(self, ...)
+            end)
+            setreadonly(mt, true)
+        end
