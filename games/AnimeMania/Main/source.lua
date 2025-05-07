803bf106655a0778ba2318fb0a2becf7517a2831
Lmy77 <lmadeira657@gmail.com>
2025-05-07 19:20:26 -0300

    Update source.lua

--- games/AnimeMania/Main/source.lua
+++ games/AnimeMania/Main/source.lua
@@ -1,60 +1,61 @@
--- bypass
-local remoteName = "Ban"
-local mt = getrawmetatable(game)
-local lscript = game:GetService("Players").LocalPlayer.Backpack:WaitForChild("ClientMain")
-local senv = getsenv(lscript)
+-- detect service
+local UserInputService = game:GetService("UserInputService")
+if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
+	print("Mobile device")
+elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
+	print("Computer device")
+end
 
-setreadonly(mt, false)
-  local oldNamecall = mt.__namecall
-  mt.__namecall = newcclosure(function(self, ...)
-      local method = getnamecallmethod()
-      if method == "FireServer" and tostring(self) == remoteName then
-          return
-      end
-      return oldNamecall(self, ...)
-  end)
 
-  hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
-      return nil
-  end))
-  mt.__namecall = newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-    if self == game.Players.LocalPlayer and method == "Kick" then
-        return nil
-    end
-    return oldNamecall(self, ...)
-  end)
-setreadonly(mt, true)
+
+-- start
+print[[                                                                     
+ /$$$$$$            /$$$$$$  /$$           /$$   /$$               /$$   /$$
+|_  $$_/           /$$__  $$|__/          |__/  | $$              | $$  / $$
+  | $$   /$$$$$$$ | $$  \__/ /$$ /$$$$$$$  /$$ /$$$$$$   /$$   /$$|  $$/ $$/
+  | $$  | $$__  $$| $$$$    | $$| $$__  $$| $$|_  $$_/  | $$  | $$ \  $$$$/ 
+  | $$  | $$  \ $$| $$_/    | $$| $$  \ $$| $$  | $$    | $$  | $$  >$$  $$ 
+  | $$  | $$  | $$| $$      | $$| $$  | $$| $$  | $$ /$$| $$  | $$ /$$/\  $$
+ /$$$$$$| $$  | $$| $$      | $$| $$  | $$| $$  |  $$$$/|  $$$$$$$| $$  \ $$
+|______/|__/  |__/|__/      |__/|__/  |__/|__/   \___/   \____  $$|__/  |__/
+                                                         /$$  | $$          
+                                                        |  $$$$$$/          
+                                                         \______/           
+]]
 
 
 
 -- variables
+function GetGears()
+  local gearName = {}
+  for _, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Gear_Shop.Frame.ScrollingFrame:GetChildren()) do
+    if v:IsA("Frame") and not v.Name:match("_Padding$") and v.Name ~= 'ItemPadding' then
+      table.insert(gearName, v.Name)
+    end
+  end
+  return gearName
+end
+function GetSeed()
+  local seedName = {}
+  for _, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Seed_Shop.Frame.ScrollingFrame:GetChildren()) do
+    if v:IsA("Frame") and not v.Name:match("_Padding$") and v.Name ~= 'ItemPadding' then
+      table.insert(seedName, v.Name)
+    end
+  end
+  return seedName
+end
 function gradient(text, startColor, endColor)
   local result = ""
   local length = #text
-
   for i = 1, length do
       local t = (i - 1) / math.max(length - 1, 1)
       local r = math.floor((startColor.R + (endColor.R - startColor.R) * t) * 255)
       local g = math.floor((startColor.G + (endColor.G - startColor.G) * t) * 255)
       local b = math.floor((startColor.B + (endColor.B - startColor.B) * t) * 255)
-
       local char = text:sub(i, i)
       result = result .. "<font color=\"rgb(" .. r ..", " .. g .. ", " .. b .. ")\">" .. char .. "</font>"
   end
-
   return result
-end
-local function moveMouseAndClick(button)
-  if button then
-      local buttonPosition = button.AbsolutePosition
-      local buttonSize = button.AbsoluteSize
-      local centerX = buttonPosition.X + (buttonSize.X / 2)
-      local centerY = buttonPosition.Y + (buttonSize.Y / 2)
-
-      mousemoveabs(centerX, centerY)
-      mouse1click()
-  end
 end
 function GetSize()
   if game:GetService('UserInputService').TouchEnabled and not game:GetService('UserInputService').KeyboardEnabled and not game:GetService('UserInputService').MouseEnabled then
@@ -66,24 +67,31 @@ end
 scriptVersion = "3.2a"
 
 
--- source
+
+-- make library
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 WindUI:Popup({
-  Title = 'Welcome to ' .. gradient("InfinityX", Color3.fromRGB(129, 63, 214), Color3.fromRGB(63, 61, 204)),
+  Title = "Welcome to " .. gradient("InfinityX", Color3.fromRGB(129, 63, 214), Color3.fromRGB(63, 61, 204)),
   Icon = "info",
-  Content = game.Players.LocalPlayer.Name .. " welcome to the script.\nI hope you enjoy using the script.",
+  Content = game.Players.LocalPlayer.Name .. " I hope you enjoy the experience\nHave fun!",
   Buttons = {
       {
-          Title = "Continue",
-          Callback = function() confirmed = true end,
-          Icon = "arrow-right",
-          Variant = "Primary",
+          Title = "Cancel",
+          --Icon = "",
+          Callback = function() end,
+          Variant = "Tertiary",
       },
+      {
+          Title = "Continue",
+          Icon = "arrow-right",
+          Callback = function() Confirmed = true end,
+          Variant = "Primary",
+      }
   }
 })
 
-repeat task.wait() until confirmed == true
+repeat wait() until Confirmed
 
 local Window = WindUI:CreateWindow({
   Title = "InfinityX - "..scriptVersion,
@@ -121,32 +129,48 @@ local Tabs = {
     Icon = "banknote",
     Desc = "AutoFarm tab",
   }),
-  Misc = Window:Tab({
-    Title = "Misc",
-    Icon = "layers",
-    Desc = "Misc tab",
+  Shop = Window:Tab({
+    Title = "Shop",
+    Icon = "hand-coins",
+    Desc = "Shop tab",
   }),
 }
 Window:SelectTab(1)
 
+
+
 -- source
 local Section = Tabs.AutoFarm:Section({
-  Title = "Clear Wave",
+  Title = "Farm Options",
   TextXAlignment = "Center",
   TextSize = 17,
 })
 local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "Teleport all mobs",
-  Desc = "Activate to teleport to all mobs",
+  Title = "Auto plant",
+  Desc = "Activate to plant all your plants",
   Icon = "check",
   Value = false,
   Callback = function(state)
-    farm = state
-    while farm do task.wait()
-      for _, v in pairs(workspace.Living:GetChildren()) do
-        if v:IsA('Model') and not game.Players:FindFirstChild(v.Name) then
-          if v:FindFirstChild('HumanoidRootPart') then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0, 5.5, 0) * CFrame.Angles(math.rad(270), 0, 0)
+    plant = state
+    while plant do task.wait()
+      for _, v in pairs(workspace.Farm:GetDescendants()) do
+        if v:IsA('StringValue') and v.Value == game.Players.LocalPlayer.Name then
+          for _, tool in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+              if tool:IsA('Tool') and tool.Name:lower():find('seed') then
+                  game.Players.LocalPlayer.Character.Humanoid:EquipTool(tool)
+                  task.wait(0.1)
+              end
+          end
+          local equipped = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool")
+          if equipped then
+              local name = equipped.Name
+              local crop = name:match("^(%w+)%s+Seed")
+              if crop then
+                  local plantPosition = v.Parent.Parent.Plant_Locations.Can_Plant.Position
+                  local ohVector31 = Vector3.new(plantPosition.X, plantPosition.Y, plantPosition.Z)
+                  local ohString2 = crop
+                  game:GetService("ReplicatedStorage").GameEvents.Plant_RE:FireServer(ohVector31, ohString2)
+              end
           end
         end
       end
@@ -154,238 +178,117 @@ local Toggle = Tabs.AutoFarm:Toggle({
   end,
 })
 local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "Use all skills + m1",
-  Desc = "When activated, the player uses all skills and m1",
+  Title = "Auto collect",
+  Desc = "Activate to collect all your plants",
   Icon = "check",
   Value = false,
   Callback = function(state)
-    skills = state
-    while skills do task.wait()
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Light'}, false)
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Skill', 1})
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Skill', 2})
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Skill', 3})
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Skill', 4})
-    end
-  end,
-})
-local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "Auto team assist",
-  Desc = "Use the team assistant automatically",
-  Icon = "check",
-  Value = false,
-  Callback = function(state)
-    assist = state
-    while assist do task.wait()
-      local args = {[1] = {[1] = "Skill", [2] = "TeamAssist"}}
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer(unpack(args))
-    end
-  end,
-})
-local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "God mode",
-  Desc = "Make the character immortal",
-  Icon = "check",
-  Value = false,
-  Callback = function(state)
-    godm = state
-    while godm do task.wait()
-      local clCheck = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("clCheck")
-      local character = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
-      local humanoid = character:FindFirstChildWhichIsA("Humanoid")
-      if humanoid and humanoid.Health ~= 0 then
-        clCheck:InvokeServer('Dash')
-      end
-    end
-  end,
-})
-local Section = Tabs.AutoFarm:Section({
-  Title = "Settings",
-  TextXAlignment = "Center",
-  TextSize = 17,
-})
-local Dropdown = Tabs.AutoFarm:Dropdown({
-  Title = "Select the number of characters",
-  Desc = "Select the number of characters you have equipped",
-  Value = "nil",
-  Multi = false,
-  AllowNone = true,
-  Values = {
-    "1", "2", "3"
-  },
-  Callback = function(Options)
-    numbersOfCharacters = Options
-  end
-})
-local Dropdown = Tabs.AutoFarm:Dropdown({
-  Title = "Select the character",
-  Desc = "Select the character you want to use",
-  Value = "nil",
-  Multi = false,
-  AllowNone = true,
-  Values = {
-    "1", "2", "3"
-  },
-  Callback = function(Options)
-    characterSelected = Options
-  end
-})
-local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "Auto select",
-  Desc = "Selects the chosen character for you",
-  Icon = "check",
-  Value = false,
-  Callback = function(state)
-    autoSelect = state
-    if autoSelect then
-        print(numbersOfCharacters)
-        print(characterSelected)
-    end
-    game.Players.LocalPlayer.PlayerGui.ChildAdded:Connect(function(child)
-        if autoSelect then
-            if child:IsA('ScreenGui') and child.Name == 'SelectedCharacter' then
-                wait(0.5)
-                child.LocalScript.Disabled = true
-
-                if numbersOfCharacters == '1' then
-                    if characterSelected == '1' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[3]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                end
-
-                if numbersOfCharacters == '2' then
-                    if characterSelected == '1' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[3]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                    if characterSelected == '2' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[4]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                end
-
-                if numbersOfCharacters == '3' then
-                    if characterSelected == '1' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[3]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                    if characterSelected == '2' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[4]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                    if characterSelected == '3' then
-                        wait(1.5)
-                        local button1 = child.Characters:GetChildren()[5]
-                        button1.Size = UDim2.new(10, 0, 10, 0)
-                        moveMouseAndClick(button1)
-                    end
-                end
-            end
-        end
-    end)
-  end,
-})
-local Toggle = Tabs.AutoFarm:Toggle({
-  Title = "Auto replay",
-  Desc = "Press the replay button every time it appears",
-  Icon = "check",
-  Value = false,
-  Callback = function(state)
-    replay = state
-    game.Players.LocalPlayer.PlayerGui.ChildAdded:Connect(function(child)
-      if replay then
-        if child:IsA('ScreenGui') and child.Name == 'Result' then
-          for _, v in pairs(child:GetDescendants()) do
-            if v:IsA('ImageButton') and v.Name == 'Replay' then
-              v.Size = UDim2.new(10, 0, 10, 0)
-              moveMouseAndClick(v)
+    collect = state
+    if collect then
+      for _, v in pairs(workspace.Farm:GetDescendants()) do
+        if v:IsA('StringValue') and v.Value == game.Players.LocalPlayer.Name then
+          for _, x in pairs(v.Parent.Parent.Plants_Physical:GetDescendants()) do
+            if x:IsA('ProximityPrompt') and x.Name == 'ProximityPrompt' then
+              x.MaxActivationDistance = math.huge
             end
           end
         end
       end
-    end)
+    end
+    while collect do task.wait()
+      for _, v in pairs(workspace.Farm:GetDescendants()) do
+        if v:IsA('StringValue') and v.Value == game.Players.LocalPlayer.Name then
+          for _, x in pairs(v.Parent.Parent.Plants_Physical:GetDescendants()) do
+            if x:IsA('ProximityPrompt') and x.Name == 'ProximityPrompt' then
+              x.MaxActivationDistance = math.huge
+              wait(.2)
+              fireproximityprompt(x)
+            end
+          end
+        end
+      end
+    end
   end,
 })
-local Button = Tabs.AutoFarm:Button({
-  Title = "Don't you get it? Click here",
-  Callback = function()
-    local Dialog = Window:Dialog({
-      Icon = "droplet",
-      Title = "How To Use",
-      Content = "In the first dropdown you will put how many characters you have equipped (Example: 2). In the second dropdown you'll put which character you want him to click on to equip in the Arena (Example: I'm equipped with Sukuna and Tsunade, in the first dropdown I'll put 2, I want him to use sukuna, in the second dropdown I'll put it as 1)",
-      Buttons = {
-        {
-          Title = "OK!", 
-          Callback = function() end,
-          Variant = "Primary"
-        }
-      }
-    }) Dialog:Open()
-  end
-})
-local Section = Tabs.Misc:Section({
-  Title = "Misc Options",
-  TextXAlignment = "Center",
-  TextSize = 17,
-})
-local Toggle = Tabs.Misc:Toggle({
-  Title = "Anti afk",
-  Desc = "Dont have kiked at 20 minutes idled",
+local Toggle = Tabs.AutoFarm:Toggle({
+  Title = "Auto sell",
+  Desc = "Activate to auto sell your plants",
   Icon = "check",
   Value = false,
   Callback = function(state)
-    afk = state
-    if afk then
-      local VirtualUser = game:GetService("VirtualUser")
-      game:GetService("Players").LocalPlayer.Idled:Connect(function()
-        VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-        task.wait(1)
-        VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-      end)
+    sell = state
+    while sell do task.wait()
+      game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(62, 3, 0)
+      game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("Sell_Inventory"):FireServer()
     end
   end,
 })
-local Button = Tabs.Misc:Button({
-  Title = "Reedem all codes",
-  Callback = function()
-    local codes = {'WelcomeNewAnimeManiaPlayers!', 'THANKSFOR175KLIKES', 'SOLOLEVELINGBUFFS', 'MONEYMONEY', 'FIRSTFREECODE'}
-    for _, v in ipairs(codes) do
-      local args = {
-          [1] = v
-      }
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SubmitCode"):InvokeServer(unpack(args))
-      task.wait(0.5)
-    end
-  end
-})
-local Section = Tabs.Misc:Section({
-  Title = "Skills Options",
+local Section = Tabs.Shop:Section({
+  Title = "Buy Gears",
   TextXAlignment = "Center",
   TextSize = 17,
 })
-local skills = {1, 2, 3, 4}
-for _, v in pairs(skills) do
-  local Toggle = Tabs.Misc:Toggle({
-    Title = "Auto use " .. v .. " skill",
-    Desc = "Click to use " .. v .. " skill automatically",
-    Icon = "check",
-    Value = false,
-    Callback = function(state)
-      local skillsSelected = state
-      while skillsSelected do task.wait()
-        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Input"):FireServer({'Skill', v})
-      end
-    end,
-  })
-end
+local Dropdown = Tabs.Shop:Dropdown({
+  Title = "Select gear",
+  Desc = "Select the gear you want to buy",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = GetGears(),
+  Callback = function(Options)
+    gearSelected = Options
+  end
+})
+local Button = Tabs.Shop:Button({
+  Title = "Buy gear",
+  Callback = function()
+    local ohString1 = gearSelected
+    game:GetService("ReplicatedStorage").GameEvents.BuyGearStock:FireServer(ohString1)
+  end
+})
+local Section = Tabs.Shop:Section({
+  Title = "Buy Seeds",
+  TextXAlignment = "Center",
+  TextSize = 17,
+})
+local Dropdown = Tabs.Shop:Dropdown({
+  Title = "Select seed",
+  Desc = "Select the seed you want to buy",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = GetSeed(),
+  Callback = function(Options)
+    seedSelected = Options
+  end
+})
+local Button = Tabs.Shop:Button({
+  Title = "Buy seed",
+  Callback = function()
+    local ohString1 = seedSelected
+    game:GetService("ReplicatedStorage").GameEvents.BuySeedStock:FireServer(ohString1)
+  end
+})
+local Section = Tabs.Shop:Section({
+  Title = "Buy Eggs",
+  TextXAlignment = "Center",
+  TextSize = 17,
+})
+local Dropdown = Tabs.Shop:Dropdown({
+  Title = "Select egg",
+  Desc = "Select the egg you want to buy",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = {1 , 2, 3},
+  Callback = function(Options)
+    eggSelected = Options
+  end
+})
+local Button = Tabs.Shop:Button({
+  Title = "Buy egg",
+  Callback = function()
+    local ohNumber1 = eggSelected
+    game:GetService("ReplicatedStorage").GameEvents.BuyPetEgg:FireServer(ohNumber1)
+  end
+})
