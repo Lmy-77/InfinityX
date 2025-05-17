d5d629bdb0b07bbf6960d63c373cab0b11e46fb4
Lmy77 <lmadeira657@gmail.com>
2025-05-17 15:23:21 -0300

    Update lobby.lua

--- games/AnimeSaga/lobby.lua
+++ games/AnimeSaga/lobby.lua
@@ -2,6 +2,15 @@
 local places = {
   Lobby = 17850641257
 }
+local rollSettings = {
+  rollAmount = nil,
+  delayToOpen = 0.5,
+}
+local teleportSettings = {
+  SelectedWorld = nil,
+  SelectedAct = nil,
+  SelectedDifficulty = nil
+}
 function gradient(text, startColor, endColor)
   local result = ""
   local length = #text
@@ -87,6 +96,11 @@ local Tabs = {
     Icon = "layers",
     Desc = "Lobby tab",
   }),
+  Join = Window:Tab({
+    Title = "| Join",
+    Icon = "merge",
+    Desc = "Join tab",
+  }),
 }
 Window:SelectTab(1)
 
@@ -98,7 +112,6 @@ local Section = Tabs.Lobby:Section({
   TextXAlignment = "Center",
   TextSize = 17,
 })
-local rollAmount = nil
 local Dropdown = Tabs.Lobby:Dropdown({
   Title = "Select amount",
   Desc = "Select the amount of roll you want",
@@ -107,7 +120,7 @@ local Dropdown = Tabs.Lobby:Dropdown({
   AllowNone = true,
   Values = {1, 10},
   Callback = function(Options)
-    rollAmount = Options
+    rollSettings.rollAmount = Options
   end
 })
 local Toggle = Tabs.Lobby:Toggle({
@@ -118,10 +131,23 @@ local Toggle = Tabs.Lobby:Toggle({
   Callback = function(state)
     roll = state
     while roll do task.wait()
-      local ohNumber1 = rollAmount
+      local ohNumber1 = rollSettings.rollAmount
       game:GetService("ReplicatedStorage").Event.Summon:FireServer(ohNumber1)
+      wait(rollSettings.delayToOpen)
     end
   end,
+})
+local Slider = Tabs.Lobby:Slider({
+  Title = "Set delay",
+  Step = 0.10,
+  Value = {
+      Min = 0,
+      Max = 5,
+      Default = 0.5,
+  },
+  Callback = function(value)
+    rollSettings.delayToOpen = value
+  end
 })
 local Section = Tabs.Lobby:Section({
   Title = "Misc",
@@ -137,12 +163,92 @@ local Button = Tabs.Lobby:Button({
 })
 local Button = Tabs.Lobby:Button({
   Title = "Reedem all codes",
-  Desc = "Clicl here to reedem all codes",
+  Desc = "Click here to reedem all codes",
   Callback = function()
     local codes = {'10MVisits', '200kMembers', 'InBugSagaWeTrust', 'BugSaga', '1MVisits', '50KActive', 'SorryForShutdown', 'SorryForDelay', 'Release'}
     for _, v in pairs(codes) do
       local ohString1 = v
-      game:GetService("ReplicatedStorage").Lobby.Codes:FireServer(ohString1)
+      game:GetService("ReplicatedStorage").Event.Codes:FireServer(ohString1)
     end
+  end
+})
+local Section = Tabs.Join:Section({
+  Title = "Auto Join Normal",
+  TextXAlignment = "Center",
+  TextSize = 17,
+})
+local Dropdown = Tabs.Join:Dropdown({
+  Title = "Select world",
+  Desc = "Select the world you want to join",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = {'Leaf Village', 'Marine Island', 'Red Light District', 'West City'},
+  Callback = function(Options)
+    teleportSettings.SelectedWorld = Options
+    if teleportSettings.SelectedWorld == 'Leaf Village' then
+      teleportSettings.SelectedWorld = 1
+    elseif teleportSettings.SelectedWorld == 'Marine Island' then
+      teleportSettings.SelectedWorld = 2
+    elseif teleportSettings.SelectedWorld == 'Red Light District' then
+      teleportSettings.SelectedWorld = 3
+    elseif teleportSettings.SelectedWorld == 'West City' then
+      teleportSettings.SelectedWorld = 4
+    end
+  end
+})
+local Dropdown = Tabs.Join:Dropdown({
+  Title = "Select act",
+  Desc = "Select the act you want",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = {'Act 1', 'Act 2', 'Act 3', 'Act 4', 'Act 5'},
+  Callback = function(Options)
+    teleportSettings.SelectedAct = Options
+    if teleportSettings.SelectedAct == 'Act 1' then
+      teleportSettings.SelectedAct = 1
+    elseif teleportSettings.SelectedAct == 'Act 2' then
+      teleportSettings.SelectedAct = 2
+    elseif teleportSettings.SelectedAct == 'Act 3' then
+      teleportSettings.SelectedAct = 3
+    elseif teleportSettings.SelectedAct == 'Act 4' then
+      teleportSettings.SelectedAct = 4
+    elseif teleportSettings.SelectedAct == 'Act 5' then
+      teleportSettings.SelectedAct = 5
+    end
+  end
+})
+local Dropdown = Tabs.Join:Dropdown({
+  Title = "Select difficulty",
+  Desc = "Select the difficulty you want",
+  Value = "nil",
+  Multi = false,
+  AllowNone = true,
+  Values = {'Normal', 'Hard', 'Nightmare'},
+  Callback = function(Options)
+    teleportSettings.SelectedDifficulty = Options
+    if teleportSettings.SelectedDifficulty == 'Normal' then
+      teleportSettings.SelectedDifficulty = 1
+    elseif teleportSettings.SelectedDifficulty == 'Hard' then
+      teleportSettings.SelectedDifficulty = 2
+    elseif teleportSettings.SelectedDifficulty == 'Nightmare' then
+      teleportSettings.SelectedDifficulty = 3
+    end
+  end
+})
+local Button = Tabs.Join:Button({
+  Title = "Create lobby + join",
+  Desc = "Click to create a lobby",
+  Callback = function()
+    local ohString1 = "Create"
+    local ohString2 = "Story"
+    local ohNumber3 = teleportSettings.SelectedWorld
+    local ohNumber4 = teleportSettings.SelectedAct
+    local ohNumber5 = teleportSettings.SelectedDifficulty
+    local ohBoolean6 = false
+    game:GetService("ReplicatedStorage").Event.JoinRoom:FireServer(ohString1, ohString2, ohNumber3, ohNumber4, ohNumber5, ohBoolean6)
+    wait(0.2)
+    
   end
 })
