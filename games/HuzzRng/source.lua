7717e67c68699805c790e4a8f38094d22bb9759f
Lmy77 <lmadeira657@gmail.com>
2025-05-26 21:38:40 -0300

    Update source.lua

--- games/HuzzRng/source.lua
+++ games/HuzzRng/source.lua
@@ -1,3 +1,20 @@
+-- start
+print[[                                                                     
+ /$$$$$$            /$$$$$$  /$$           /$$   /$$               /$$   /$$
+|_  $$_/           /$$__  $$|__/          |__/  | $$              | $$  / $$
+  | $$   /$$$$$$$ | $$  \__/ /$$ /$$$$$$$  /$$ /$$$$$$   /$$   /$$|  $$/ $$/
+  | $$  | $$__  $$| $$$$    | $$| $$__  $$| $$|_  $$_/  | $$  | $$ \  $$$$/ 
+  | $$  | $$  \ $$| $$_/    | $$| $$  \ $$| $$  | $$    | $$  | $$  >$$  $$ 
+  | $$  | $$  | $$| $$      | $$| $$  | $$| $$  | $$ /$$| $$  | $$ /$$/\  $$
+ /$$$$$$| $$  | $$| $$      | $$| $$  | $$| $$  |  $$$$/|  $$$$$$$| $$  \ $$
+|______/|__/  |__/|__/      |__/|__/  |__/|__/   \___/   \____  $$|__/  |__/
+                                                         /$$  | $$          
+                                                        |  $$$$$$/          
+                                                         \______/           
+]]
+
+
+
 -- variables
 local infiniteSpinSettings = {
   Keep = false,
@@ -302,6 +319,118 @@ local Toggle = Tabs.Op:Toggle({
         end
         task.wait()
       until triggered
+
+      game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+    end
+  end,
+})
+local Toggle = Tabs.Op:Toggle({
+  Title = "Start + Auto execute",
+  Icon = "check",
+  Value = false,
+  Callback = function(state)
+    autoExecute = state
+    queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+    if autoExecute then
+      if infiniteSpinSettings.Delete == false and infiniteSpinSettings.Keep == false then
+        local Dialog = Window:Dialog({
+          Icon = "droplet",
+          Title = "Prompt",
+          Content = "Please activate any of the toggles in order to run the code correctly",
+          Buttons = {
+            {
+              Title = "OK!", 
+              Callback = function() end,
+              Variant = "Primary"
+            }
+          }
+        }) Dialog:Open()
+        return
+      end
+      if infiniteSpinSettings.Delete == true and infiniteSpinSettings.Keep == true then
+        local Dialog = Window:Dialog({
+          Icon = "droplet",
+          Title = "Prompt",
+          Content = "You can only leave one of the options below selected, not both",
+          Buttons = {
+            {
+              Title = "OK!", 
+              Callback = function() end,
+              Variant = "Primary"
+            }
+          }
+        }) Dialog:Open()
+        return
+      end
+
+      if game:GetService("Players").LocalPlayer.PlayerGui.Game.MainRoll.Main_RollingFrame.SkipAnim.Status.Text == 'OFF' then
+        local Signals = {"Activated", "MouseButton1Down", "MouseButton2Down", "MouseButton1Click", "MouseButton2Click"}
+        for _,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Game.MainRoll.Main_RollingFrame.SkipAnim:GetChildren()) do
+          if v:IsA("TextButton") and v.Name == 'Button' then
+            for i,Signal in pairs(Signals) do
+              firesignal(v[Signal])
+            end
+          end
+        end
+      end
+
+      wait(2)
+
+      local mt = getrawmetatable(game)
+      setreadonly(mt, false)
+        local oldNamecall = mt.__namecall
+        hookfunction(game.Players.LocalPlayer.Kick, newcclosure(function(...)
+            return nil
+        end))
+        mt.__namecall = newcclosure(function(self, ...)
+            local method = getnamecallmethod()
+            if self == game.Players.LocalPlayer and method == "Kick" then
+                return nil
+            end
+            return oldNamecall(self, ...)
+        end)
+      setreadonly(mt, true)
+
+      game:GetService("Players").LocalPlayer.PlayerScripts["__Fdd_V22_#"]:Destroy()
+      game:GetService("Players").LocalPlayer.PlayerScripts["__GLB2_V1_A4$#"]:Destroy()
+      game:GetService("Players").LocalPlayer.PlayerScripts["__GLB_V1_#!@"]:Destroy()
+      game:GetService("Players").LocalPlayer.PlayerScripts["___LSLC_V1_##@"]:Destroy()
+      game:GetService("Players").LocalPlayer.PlayerScripts["__binSvc_v2\""]:Destroy()
+
+      local rolls = game:GetService("Players").LocalPlayer.leaderstats:WaitForChild("Rolls")
+      local target = rolls.Value + 450
+      local triggered = false
+      rolls:GetPropertyChangedSignal("Value"):Connect(function()
+          if rolls.Value >= target then
+            triggered = true
+            game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
+          end
+      end)
+      repeat task.wait()
+        game:GetService("ReplicatedStorage"):WaitForChild("L5_z%Q1!Rx_"):FireServer()
+        if infiniteSpinSettings.Delete == true then
+          local args = {
+            "delete"
+          }
+          game:GetService("ReplicatedStorage"):WaitForChild("ConfirmAura"):FireServer(unpack(args))
+        end
+        if infiniteSpinSettings.Keep == true then
+          local args = {
+            "keep"
+          }
+          game:GetService("ReplicatedStorage"):WaitForChild("ConfirmAura"):FireServer(unpack(args))
+        end
+        task.wait()
+      until triggered
+
+
+      local TeleportCheck = false
+      Game.Players.LocalPlayer.OnTeleport:Connect(function(State)
+        if (not TeleportCheck) and queueteleport then
+          TeleportCheck = true
+          queueteleport('wait(0.5) loadstring(game:HttpGet("https://gitlab.com/Lmy77/menu/-/raw/main/infinityx"))()')
+        end
+      end)
 
       game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
     end
