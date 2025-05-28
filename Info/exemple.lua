48a6a9152099785076fce3a992cab81d82924280
Lmy77 <lmadeira657@gmail.com>
2025-05-28 17:13:54 -0300

    Create exemple.lua

---
+++ Info/exemple.lua
@@ -0,0 +1,9 @@
+local Info = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/library/Info/source.lua", true))()
+
+
+local UI = Info:CreateWindow("InfinityX")
+UI:AddInfo("Lucky Arrows Count:", 1)
+UI:AddInfo("Balance:", 15022, Color3.fromRGB(0, 255, 170))
+UI:AddInfo("Player Username:", "wadawdawdawdawd3")
+UI:AddInfo("Centered Info", "123", nil, { align = "center", divider = true })
+UI:AddInfo("Player Username:", "wadawdawdawdawd3")
