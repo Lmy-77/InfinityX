9d78b6a9f2a3135fec0c80887660a23bcc750a03
Lmy77 <lmadeira657@gmail.com>
2025-05-31 20:55:36 -0300

    Update exemple.lua

--- Info/exemple.lua
+++ Info/exemple.lua
@@ -3,7 +3,17 @@ local Info = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/I
 
 local UI = Info:CreateWindow("InfinityX")
 UI:AddInfo("Lucky Arrows Count:", 1)
-UI:AddInfo("Balance:", 15022, Color3.fromRGB(0, 255, 170))
+UI:AddInfo("Balance:", 15022, Color3.fromRGB(2, 255, 171))
+UI:AddInfo("Balance:", 15022, Color3.fromRGB(2, 19, 255))
 UI:AddInfo("Player Username:", "wadawdawdawdawd3")
 UI:AddInfo("Centered Info", "123", nil, { align = "center", divider = true })
 UI:AddInfo("Player Username:", "wadawdawdawdawd3")
+UI:AddLabel("INFORMAÇÕES GERAIS", "center")
+
+local Info1 = UI:AddInfo("Balance", "1500")
+Info1:Update("2500")
+
+UI:AddInfo("Username", game.Players.LocalPlayer.Name, nil, { align = "center", divider = true })
+UI:AddButton("Click Me", Color3.fromRGB(255, 255, 0), function()
+    print("Botão clicado!")
+end)
