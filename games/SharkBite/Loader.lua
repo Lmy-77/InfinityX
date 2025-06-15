9e4aa792b81b13c9b6c936b0fd0e9339b9a5a515
Lmy77 <lmadeira657@gmail.com>
2025-06-15 13:27:29 -0300

    Update Loader.lua

--- games/SharkBite/Loader.lua
+++ games/SharkBite/Loader.lua
@@ -7,5 +7,5 @@ local placeId = {
 if game.PlaceId == placeId.Game1 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/SharkBite/Game1/source.lua", true))()
 elseif game.PlaceId == placeId.Game2 then
-  print('In dev')
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/InfinityX/refs/heads/scripts/games/SharkBite/Game2/source.lua", true))()
 end
